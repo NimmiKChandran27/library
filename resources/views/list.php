ed6501412251b92db312c12f64148b8df2326d69
NimmiKChandran27 <mca536@rajagiri.edu>
2021-05-31 11:44:20 +0530

    first commit

---
+++ resources/views/list.php
@@ -0,0 +1,60 @@
+<!<nav class="navbar navbar-expand-lg navbar-dark bg-success">
+            <div class="container-fluid">
+              <a class="navbar-brand" href='/'>BOOK BYCON</a>
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                  <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="/menu">MENU</a>
+                  </li>
+                  
+                  
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="/aboutus" tabindex="-1" aria-disabled="true">About Us</a>
+                  </li>
+                </ul>
+                <ul class="list-unstyled" style="font-size: 20px;color: rgb(243, 247, 243);">
+                </ul>
+              </div>
+            </div>
+          </nav>
+<br>
+<br>
+
+          <div class="container">
+        
+        <div class="row">
+            <div class="col col-12 col-sm-12 col-md-12"></div>
+
+<h1>AVAILABLE BOOKS</h1>
+
+
+<div class="list-group">
+  <label class="list-group-item">
+    <input class="form-check-input me-1" type="checkbox" value="">
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSfMq71pNrurtkS0RunJcHDW4FB5XxXHiS_ckcHCh9_B2WDIHdnVvYlsJZhdCMYsKdFqvSf2DSh&usqp=CAc">
+  </label>
+  <label class="list-group-item">
+    <input class="form-check-input me-1" type="checkbox" value="">
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTnrVZCpI3gowN58s-6z15-JWqBOhMkcBOZ63yAJwBHNV-lk480pIvSjahR1Q&usqp=CAc">
+  </label>
+ 
+  <label class="list-group-item">
+    <input class="form-check-input me-1" type="checkbox" value="">
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSteLr45l2o4m4hjZJch0kZ2E9iAu5q2eVPVa-etA8ddTUz0vdv3STKBoQY2zE&usqp=CAc">
+  </label>
+  <label class="list-group-item">
+    <input class="form-check-input me-1" type="checkbox" value="">
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQPcQlt7Bi9pqU-yy3CNehVYMT-qDiJhtCQzMXicft7dflvvNQ0g13MA6NI&usqp=CAc">
+  </label>
+</div>
+            
+           
+</div>
+</div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+</body>
+</html>
